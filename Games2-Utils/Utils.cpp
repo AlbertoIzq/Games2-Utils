bf6b52292f5d143b966cd31f726584c9ebfc4933
Alberto <alberto.izquierdomo@gmail.com>
2021-04-13 16:57:10 +0200

    Start with 2DVector functionality

--- Games2-Utils/Utils.cpp
+++ Games2-Utils/Utils.cpp
@@ -1,5 +1,12 @@
+// Needed for static library functionality
+#include "pch.h"
+#include "framework.h"
+
 #include "Utils.h"
 
 namespace Game2DUtils {
-
+	std::ostream& operator<<(std::ostream& consoleOut, const Vec2D& vec) {
+		std::cout << "X: " << vec.mX << ", Y: " << vec.mY << std::endl;
+		return consoleOut;
+	}
 }
--- Games2-Utils/Utils.h
+++ Games2-Utils/Utils.h
@@ -4,6 +4,22 @@
 #include "pch.h"
 #include "framework.h"
 
-namespace Game2DUtils {
+#include <iostream>
 
+namespace Game2DUtils {
+	class Vec2D	{
+		private:
+			float mX, mY;
+
+		public:
+			Vec2D() : Vec2D(0, 0) {}
+			Vec2D(float x, float y) : mX{ x }, mY{ y } {}
+
+			inline void setX(float x) { mX = x; }
+			inline void setY(float x) { mX = x; }
+			inline float getX() const { return mX; }
+			inline float getY() const { return mY; }
+
+			friend std::ostream& operator<<(std::ostream& consoleOut, const Vec2D& vec);
+	};
 }
